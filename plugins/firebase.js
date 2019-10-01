456edaf5cb28f063ea27f4ac24afb2ad8bae6e5a
永田悠飛 <>
2019-10-01 20:47:13 +0900

    Fix about nuxt project process.env

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,13 +2,13 @@ const firebase = require('firebase/app');
 require('firebase/firestore');
 
 const config = {
-    apiKey: process.env.FB_API_KEY,
-    authDomain: process.env.FB_AUTH_DOMAIN,
-    databaseURL: process.env.FB_DATABASE_URL,
-    projectId: process.env.FB_PROJECTID,
-    storageBucket: process.env.FB_STORAGE_BUCKET,
-    messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
-    appId: process.env.FB_APP_ID
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId
 }
 
 if (!firebase.apps.length) {
